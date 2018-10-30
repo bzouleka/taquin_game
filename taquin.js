91a7da5237a5dcd186d334c811db6906e2c1045c
bzouleka <bzouleka@gmail.com>
2018-10-30 15:54:34 +0100

    melange, boutton shuffle et solutions du taquin

--- taquin.js
+++ taquin.js
@@ -15,26 +15,66 @@ function shuffle(tabT) {
     }
     return tabT;
 }
-let melange = shuffle(tabT);
-// console.log(melange);
-afficher(melange);
 
-function paire(tabT){
+
+function permutation(tabT){
     let nbrPermutation = 0;
-    for (let i = 0; i < tabT.length -1; i++){
+    for (let i = 0; i < tabT.length ; i++){
         let j = i;
         while( j < tabT.length -1){
-            if (tabT[j]+1 < tabT[j] ){
-                let temp= tabT[i];
-                tabT[i] = tabT[j];
-                tabT[j] = temp;
+            if (tabT[j+1] > tabT[j] ){ //comparaison
+                //Ne pas faire de swap pour chercher le nombre de parité
+
+                nbrPermutation++;
             }
+            j++
         }
     }
-    return tabT;
-    console.log("parité " + j)
+    return nbrPermutation;
+}
+function paire(melange) {
+    let paire = permutation(melange);
+    if (paire % 2 === 0) {
+
+        console.log("Il y a " + paire + " permutations.");
+        return true;
+        // permutation(melange);
+    } else {
+        console.log("Pas de Taquin dispo!!!");
+        return false;
+    }
+}
+
+function start() {
+    let t = shuffle(tabT);
+    afficher(t);
+    if (paire(t) === true) {
+        paire(t);
+        document.getElementById('message').innerHTML = ("Good luck!!");
+    } else {
+        document.getElementById('message').innerHTML = ("Pas de solution valide!!");
+    }
+}
+
+
+function move(){
+
+    let vide = ' ';
+    let G = document.getElementById("gauche");
+    let D = document.getElementById("droite");
+
+    for (let i = 0; i < tabT.length; i++){
+        if (vide){}
+
+
+    }
 
 }
+
+
+
+
+
 
 
 
